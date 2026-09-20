1567624e3fa7555e0e81cde6fd59bb13d1003ac1
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-20 11:53:04 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -9,17 +9,16 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'>
-      {/* <DarkModeButton/> */}
+    // 这里的 pt-4 是上面留一点空间，pb-1 是下面只留极小空间，彻底解决底部过宽的问题
+    <footer className='relative z-10 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm pt-4 pb-1'>
       <i className='fas fa-copyright' /> {`${copyrightDate}`}
       <span>
         <i className='mx-1 animate-pulse fas fa-heart' />
         <a
           href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '>
+          className='underline font-bold dark:text-gray-300'>
           {siteConfig('AUTHOR')}
         </a>
-        <br />
         <BeiAnSite />
         <span className='hidden busuanzi_container_site_pv'>
           <i className='fas fa-eye' />
@@ -30,7 +29,6 @@ const Footer = ({ title }) => {
           <span className='px-1 busuanzi_value_site_uv'> </span>
         </span>
       </span>
-      <br />
     </footer>
   )
 }
