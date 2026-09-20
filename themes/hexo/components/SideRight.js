bfe76aed6acde183159086594fb62bc56fbcafe4
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-20 19:18:17 +0800

    Update SideRight.js

--- themes/hexo/components/SideRight.js
+++ themes/hexo/components/SideRight.js
@@ -56,7 +56,7 @@ export default function SideRight(props) {
   return (
     <div
       id='sideRight'
-      className={` lg:w-80 lg:pt-8 ${post ? 'lg:pt-0' : 'lg:pt-4'}`}>
+      className='lg:w-80 pt-0 lg:pt-0'>
       <div className='sticky top-8 space-y-4'>
         {post && post.toc && post.toc.length > 1 && (
           <Card>
