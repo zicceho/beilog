256cd01a14f4148b8220362ecff3775490a12bae
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-22 15:25:51 +0800

    Update BlogPostCard.js

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -17,7 +17,6 @@ const parseExt = (ext) => {
 }
 
 const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
-  if (typeof window !== 'undefined') console.log('POST.AUDIO:', post?.audio)
   const showPreview =
     siteConfig('HEXO_POST_LIST_PREVIEW', null, CONFIG) && post.blockMap
   if (post && !post.pageCoverThumbnail && siteConfig('HEXO_POST_LIST_COVER_DEFAULT', null, CONFIG)) {
@@ -28,7 +27,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
     post?.pageCoverThumbnail &&
     !showPreview
 
-  const audioUrl = parseExt(post.ext)
+  const audioUrl = post?.audio || parseExt(post.ext)
   const [isPlaying, setIsPlaying] = useState(false)
 
   useEffect(() => {
