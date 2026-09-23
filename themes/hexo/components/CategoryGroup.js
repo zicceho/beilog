a3f379b37fcbee215b5f4cc5eb79af7968a485c8
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-23 23:43:41 +0800

    Update CategoryGroup.js

--- themes/hexo/components/CategoryGroup.js
+++ themes/hexo/components/CategoryGroup.js
@@ -1,30 +1,46 @@
 import SmartLink from '@/components/SmartLink'
+import { useGlobal } from '@/lib/global'
+import { getCategoryUrl } from '@/lib/utils/category'
 
 const CategoryGroup = ({ currentCategory, categories }) => {
+  const { NOTION_CONFIG } = useGlobal()
+
   if (!categories) {
     return <></>
   }
-  return <>
-    <div id='category-list' className='dark:border-gray-600 flex flex-wrap  mx-4'>
-      {categories.map(category => {
-        const selected = currentCategory === category.name
-        return (
-          <SmartLink
-            key={category.name}
-            href={`/category/${category.name}`}
-            passHref
-            className={(selected
-              ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
-              : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
-              '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
-
-            <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name}({category.count})</div>
-
-          </SmartLink>
-        );
-      })}
-    </div>
-  </>;
+  return (
+    <>
+      <div
+        id='category-list'
+        className='dark:border-gray-600 flex flex-wrap  mx-4'>
+        {categories.map(category => {
+          const selected = currentCategory === category.name
+          return (
+            <SmartLink
+              key={category.name}
+              href={getCategoryUrl(category.name, NOTION_CONFIG)}
+              passHref
+              className={
+                (selected
+                  ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
+                  : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
+                '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'
+              }>
+              <div>
+                {' '}
+                <i
+                  className={`mr-2 fas ${
+                    selected ? 'fa-folder-open' : 'fa-folder'
+                  }`}
+                />
+                {category.name}({category.count})
+              </div>
+            </SmartLink>
+          )
+        })}
+      </div>
+    </>
+  )
 }
 
 export default CategoryGroup
