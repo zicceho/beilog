bdf709fc17abd79385ad13ce96f4415e086d1d79
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-24 22:31:51 +0800

    Update index.js

--- pages/episodes/index.js
+++ pages/episodes/index.js
@@ -1,9 +1,7 @@
 import BLOG from '@/blog.config'
 import { siteConfig } from '@/lib/config'
 import { fetchGlobalAllData } from '@/lib/db/SiteDataApi'
-import { isBrowser } from '@/lib/utils'
 import { DynamicLayout } from '@/themes/theme'
-import { useEffect } from 'react'
 import {
   EPISODES_PER_PAGE,
   sortEpisodes,
@@ -16,22 +14,6 @@ import {
  * @returns
  */
 const EpisodesIndex = props => {
-  // 注意：此处的 hash 滚动逻辑将在下一轮移动到 EpisodesPage.js 里，以实现分页页面共享。
-  // 目前为了保证首页功能完整，暂时保留。
-  useEffect(() => {
-    if (isBrowser) {
-      const anchor = window.location.hash
-      if (anchor) {
-        setTimeout(() => {
-          const anchorElement = document.getElementById(anchor.substring(1))
-          if (anchorElement) {
-            anchorElement.scrollIntoView({ block: 'start', behavior: 'smooth' })
-          }
-        }, 300)
-      }
-    }
-  }, [])
-
   const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
   return <DynamicLayout theme={theme} layoutName='LayoutEpisodes' {...props} />
 }
