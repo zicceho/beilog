06abed4caa6e89514f6df48b5af119c318843b7b
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-24 23:15:18 +0800

    Update BlogPostCard.js

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -17,7 +17,8 @@ const parseExt = (ext) => {
   return null
 }
 
-const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
+// <--- 新增 page, disableDateLink 两个参数
+const BlogPostCard = ({ index, post, showSummary, siteInfo, page, disableDateLink }) => {
   const router = useRouter()
   const showPreview =
     siteConfig('HEXO_POST_LIST_PREVIEW', null, CONFIG) && post.blockMap
@@ -74,6 +75,8 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
           showPageCover={showPageCover}
           showPreview={showPreview}
           showSummary={showSummary}
+          page={page}                     // <--- 传下去
+          disableDateLink={disableDateLink} // <--- 传下去
         />
 
         {showPageCover && (
