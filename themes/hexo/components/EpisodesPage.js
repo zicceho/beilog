0b207bf28fc0e2be740bcb30892618280dfd72e5
zicceho <165814501+zicceho@users.noreply.github.com>
2026-09-25 13:55:47 +0800

    Update EpisodesPage.js

--- themes/hexo/components/EpisodesPage.js
+++ themes/hexo/components/EpisodesPage.js
@@ -1,23 +1,45 @@
 import { useEffect } from 'react'
+
 import { isBrowser } from '@/lib/utils'
+
 import { siteConfig } from '@/lib/config'
 import CONFIG from '../config'
+
 import BlogPostCard from './BlogPostCard'
+
 import PaginationNumber from './PaginationNumber'
 
 const EpisodesPage = props => {
-  const { archivePosts = {}, page = 1, episodesTotalPages = 1, siteInfo, posts = [] } = props
-  const showSummary = siteConfig('HEXO_POST_LIST_SUMMARY', null, CONFIG)
+  const {
+    archivePosts = {},
+    page = 1,
+    episodesTotalPages = 1,
+    siteInfo,
+    posts = []
+  } = props
+
+  const showSummary = siteConfig(
+    'HEXO_POST_LIST_SUMMARY',
+    null,
+    CONFIG
+  )
 
   // 统一在这里处理 hash 滚动
   useEffect(() => {
     if (isBrowser) {
       const anchor = window.location.hash
+
       if (anchor) {
         setTimeout(() => {
-          const anchorElement = document.getElementById(anchor.substring(1))
+          const anchorElement = document.getElementById(
+            anchor.substring(1)
+          )
+
           if (anchorElement) {
-            anchorElement.scrollIntoView({ block: 'start', behavior: 'smooth' })
+            anchorElement.scrollIntoView({
+              block: 'start',
+              behavior: 'smooth'
+            })
           }
         }, 300)
       }
@@ -25,15 +47,22 @@ const EpisodesPage = props => {
   }, [])
 
   return (
-    <div className='w-full'>
+    <div className='w-full px-2 md:px-0'>
       {Object.keys(archivePosts).map(month => (
-        <section key={month} id={month} className='mb-8'>
+        <section
+          key={month}
+          id={month}
+          className='mb-8'>
           <div className='text-2xl font-extrabold mb-4 text-gray-700 dark:text-gray-300'>
             {month}
           </div>
+
           <div className='space-y-6'>
             {archivePosts[month].map(post => (
-              <div key={post.id} id={String(post.slug)} className='scroll-mt-24'>
+              <div
+                key={post.id}
+                id={String(post.slug)}
+                className='scroll-mt-24'>
                 <BlogPostCard
                   index={posts.indexOf(post) + 1}
                   post={post}
@@ -46,7 +75,10 @@ const EpisodesPage = props => {
         </section>
       ))}
 
-      <PaginationNumber page={page} totalPage={episodesTotalPages} />
+      <PaginationNumber
+        page={page}
+        totalPage={episodesTotalPages}
+      />
     </div>
   )
 }
